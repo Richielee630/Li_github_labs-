767e167969a756a7d331af5a8dd4d8e20aad3436
Richie Lee <zhaoqin.li@sjsu.edu>
2020-03-08 21:32:35 -0700

    3/8/2020 1

--- lib/lab06/src/doubly_linked_list.cpp
+++ lib/lab06/src/doubly_linked_list.cpp
@@ -13,6 +13,8 @@ namespace lab6{
     }
 
     doubly_linked_list::doubly_linked_list(std::vector<int> vector_input) {
+        head = nullptr;
+        tail = nullptr;
         if(!vector_input.empty()) {
             node *newnode = new node(vector_input[0]);
             head = newnode;
@@ -25,8 +27,6 @@ namespace lab6{
                 tail = n;
             }
         }
-        head = nullptr;
-        tail = nullptr;
     }
 
     doubly_linked_list::doubly_linked_list(const doubly_linked_list &original) {
@@ -74,29 +74,32 @@ namespace lab6{
     std::vector<int> doubly_linked_list::get_set(unsigned position_from, unsigned position_to) {
         node *cur_front = head;
         node *cur_back = head;
+
+        std::vector<int> output;
+
         while (position_from > 0){
             cur_front = cur_front->next;
             --position_from;
         }
-//        while (cur_front->next){
-//            std::cout << cur_front->get_data();
-//        }
+
         while (position_to > 0 ){
             cur_back = cur_back->next;
             --position_to;
         }
-        while (cur_front->next != cur_back){
-            std::cout << cur_front->get_data();
+
+        while (cur_front != cur_back){
+            output.push_back(cur_front->get_data());
+            cur_front = cur_front->next;
         }
+        output.push_back(cur_front->get_data());///当set包含position to的data时的情况
+        return output;
         ///vector 怎么 return
         ///此时如何return出两个position之间的strings？
-        std::vector<int> output;
-        output.push_back(get_data(position_from));
     }
 
     unsigned doubly_linked_list::size() {
         int len = 0;
-        node *current;
+        node *current = head;
         while (current != nullptr)
         {
             len++;
@@ -111,9 +114,15 @@ namespace lab6{
 
     void doubly_linked_list::append(int input) {
         node *new_node = new node(input);
-        tail->next = new_node;
-        new_node->prev = tail;
-        tail = new_node;
+        if(tail == nullptr)
+        {
+            head = new_node;
+            tail = head;
+        }else{
+            tail->next = new_node;
+            new_node->prev = tail;
+            tail = new_node;
+        }
     }
 
     void doubly_linked_list::insert(int input, unsigned int location) {
@@ -125,19 +134,24 @@ namespace lab6{
         if (location == 0){
             tmp->next = head;
             head = tmp;
-                if(tail == nullptr){
-                    tail = head;///这行的意义是？
-                }
-            }else{
-                node* cur = head;
-                while(location > 1){
-                    --location;
-                    cur = cur->next;
-                }
-                tmp->next = cur->next;///这一步的意义是？
-                cur->next = tmp;
-                tmp->prev = cur;
+            if(tail == nullptr){
+                tail = head;
             }
+        }
+        else {
+            node *cur = head;
+            while (location > 1) {
+                --location;
+                cur = cur->next;
+            }
+            node *next = cur->next;
+            //tmp->next = cur->next;///这一步的意义是？
+            //cur->next->prev = cur;
+            cur->next = tmp;
+            tmp->prev = cur;
+            tmp->next = next;
+            next->prev = tmp;
+        }
     }
 
     void doubly_linked_list::remove(unsigned location) {
@@ -145,11 +159,15 @@ namespace lab6{
             node* cur = head;
             if(cur != nullptr){
                 head = head->next;
+
                 if(head == nullptr){
                     tail = head;
+                }else{
+                    head->prev = nullptr;
                 }
                 delete cur;
-                cur = nullptr;///delete cur了为什么还要cur = nullptr？
+                cur = nullptr;
+
             }
         }
         else{
@@ -161,45 +179,128 @@ namespace lab6{
             if(cur->next){
                 node* get = cur->next;
                 cur->next = cur->next->next;
+                if(cur->next){
+                    cur->next->prev = cur;
+                }
                 if(get == tail){
                     tail = cur;
                 }
                 delete get;
                 get = nullptr;
-//                if(tail == get)
-//                {
-//                    tail = cur;
-//                }
             }
         }
     }
 
     doubly_linked_list doubly_linked_list::split(unsigned position) {
-        ///1.创建链表list1,2
-        ///2.location之前的放进list1，location之后的放list2
-        node *split1;
-        node *split2;
+        ///1.找到position的node
+        ///2.location之后的作为新list，定义新list的头尾ptr；
+        ///3.注意prev定义
         node *cur1 = head;
         while (position > 0){
             --position;
             cur1 = cur1->next;
         }
-        while (cur1->prev){
-            split1->get_data() = cur1->get_data();
-            cur1 = cur1->prev;
-            return;
-        }
+        doubly_linked_list new_list;
+        new_list.head = cur1;
+        new_list.tail = tail;
+        tail = cur1->prev;
+        tail->next = nullptr;
+        new_list.head->prev = nullptr;
+        return  new_list;
+//        while (cur1->prev){
+//            split1->get_data() = cur1->get_data();
+//            cur1 = cur1->prev;
+//            return;
+//        }
 
     }
 
     doubly_linked_list doubly_linked_list::split_set(unsigned position_1, unsigned position_2) {
+///        1.找到两个position对应的node
+///        2.创建新linkedlist然后把头尾ptr定义好
+///        3.创建两个pointer来检测这个list前后是否还有数据
+        node* pos_1 = head;
+        node* pos_2 = head;
+        while(position_1 > 0){
+            --position_1;
+            pos_1 = pos_1->next;
+        }
+        while(position_2 > 0){
+            --position_2;
+            pos_2 = pos_2->next;
+        }
+        doubly_linked_list new_list;
+        new_list.head = pos_1;
+        new_list.tail = pos_2;
+        node* prev = pos_1->prev;
+        node* next = pos_2->next;
+        if(prev == nullptr){
+            head = next;
+        }
+        if(next == nullptr){
+            tail = prev;
+        }
+        if(prev != nullptr)///如果新list之外还有数据，就将他们组合成另一个新list
+        {
+            prev->next = next;
+        }
+        if(next != nullptr){
+            next->prev = prev;
+        }
+        pos_1->prev = nullptr;
+        pos_2->next = nullptr;
+        return new_list;
     }
 
     void doubly_linked_list::swap(unsigned position_1, unsigned position_2) {
+    ///1.找到两个position对应的node
+    ///
+    ///
+    ///
+        node* pos_1 = head;
+        node* pos_2 = head;
+        while(position_1 > 0){
+            --position_1;
+            pos_1 = pos_1->next;
+        }
+        while(position_2 > 0){
+            --position_2;
+            pos_2 = pos_2->next;
+        }
+        node* new_node_1 = new node(pos_2->get_data());
+        node* new_node_2 = new node(pos_1->get_data());
+
+        new_node_1->prev = pos_1->prev;
+        new_node_1->next = pos_1->next;
+        new_node_2->prev = pos_2->prev;
+        new_node_2->next = pos_2->next;
+        if(pos_1->prev)
+            pos_1->prev->next = new_node_1;
+        if(pos_1->next)
+            pos_1->next->prev = new_node_1;
+        if(pos_2->prev)
+            pos_2->prev->next = new_node_2;
+        if(pos_2->next)
+            pos_2->next->prev = new_node_2;
+        if(pos_1 == head){
+            head = new_node_1;
+        }
+        if(pos_1 == tail){
+            tail = new_node_1;
+        }
+        if(pos_2 == head){
+            head = new_node_2;
+        }
+        if(pos_2 == tail){
+            tail = new_node_2;
+        }
     }
 
     void doubly_linked_list::swap_set(unsigned location_1_start, unsigned location_1_end, unsigned location_2_start,
                                       unsigned location_2_end) {
+        for(int i = 0; i+location_1_start <= location_1_end; ++i){
+            swap(location_1_start + i, location_2_start + i);
+        }
     }
 
     void doubly_linked_list::sort() {
@@ -236,10 +337,17 @@ namespace lab6{
                 }
             }
         }
+        cur = head;
+        while(cur->next){
+            cur->next->prev = cur;
+            cur = cur->next;
+        }
     }
 
     doubly_linked_list doubly_linked_list::operator+(const doubly_linked_list &rhs) const {
-
+        auto* output = new doubly_linked_list(*this);
+        *output += rhs;
+        return *output;
     }
 
     doubly_linked_list& doubly_linked_list::operator=(const doubly_linked_list &rhs) {
@@ -257,28 +365,43 @@ namespace lab6{
             }
             if (rhs.head != nullptr)
             {
+
                 node *RHS_tmp = rhs.head;
-                node *tmp = head = new node(RHS_tmp->get_data());
-                //size = RHS.size;
-                RHS_tmp = RHS_tmp->next;
                 while (RHS_tmp != nullptr)
                 {
-                    tmp->next = new node(RHS_tmp->get_data());
-                    tmp = tmp->next;
-                    tail = tmp;
-                    RHS_tmp = RHS_tmp->next;
+                    append(RHS_tmp->get_data());
                 }
-                //size = RHS.size;
             }
         }
         return *this;
-        //return <#initializer#>;
     }
 
     doubly_linked_list& doubly_linked_list::operator+=(const doubly_linked_list &rhs) {
+        node* cur = rhs.head;
+        while(cur){
+            append(cur->get_data());
+            cur = cur->next;
+        }
+        return *this;
     }
 
     bool doubly_linked_list::operator==(const doubly_linked_list &rhs) {
+        node* r = rhs.head;
+        node* c = head;
+        while(r&&c){
+            if(r->get_data()!=c->get_data()){
+                return false;
+            }
+            r = r->next;
+            c = c->next;
+        }
+        if(c && !r){
+            return false;
+        }
+        if(!c && r){
+            return false;
+        }
+        return true;
     }
 
     std::ostream &operator<<(std::ostream &stream, doubly_linked_list &RHS) {
@@ -292,9 +415,9 @@ namespace lab6{
     }
 
     std::istream &operator>>(std::istream &stream, doubly_linked_list &RHS) {
-        std::string input;
+        int input;
         stream >> input;
-        RHS.append();
+        RHS.append(input);
         return stream;
     }
 }
