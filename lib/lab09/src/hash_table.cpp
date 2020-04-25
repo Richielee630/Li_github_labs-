c15442054299de742fd07d37b9f0f5968251884f
Richielee630 <60235970+Richielee630@users.noreply.github.com>
2020-04-24 21:01:06 -0700

    4/24/2020

--- lib/lab09/src/hash_table.cpp
+++ lib/lab09/src/hash_table.cpp
@@ -24,10 +24,38 @@ namespace lab9 {
 
     void hash_table::expand() {
         // Expand the hash table by a factor of 2 every time this function is called
-
+        key_value* hash_table_array_new = new key_value[2*max_size];
+        for(int i = 0; i < max_size; ++i){
+            if(hash_table_array[i].key != ""){
+                int attempt = 0;
+                int h1 = hash_1(hash_table_array[i].key);
+                int h2 = 0;
+                if(probing == 'd'){
+                    h2 = hash_2(hash_table_array[i].key);
+                }
+                int a = 0;
+                while(true){
+                    if(hash_table_array_new[(h1+a)%(2*max_size)].key == ""){
+                        hash_table_array_new[(h1+a)%(2*max_size)].key = hash_table_array[i].key;
+                        hash_table_array_new[(h1+a)%(2*max_size)].value = hash_table_array[i].value;
+                        break;
+                    }
+                    ++attempt;
+                    a = attempt;
+                    if(probing == 'q'){
+                        a = a*a;
+                    }else if(probing == 'd'){
+                        a = a * h2;
+                    }
+                }
+            }
+        }
+        max_size = 2*max_size;
+        delete[] hash_table_array;
+        hash_table_array = hash_table_array_new;
     }
 
-    hash_table::hash_table(char type) {
+    hash_table::hash_table(char type):probing(type),max_size(100),current_size(0),hash_table_array(new key_value[100]) {
         /*
          * Define the probing technique
          * 'l': Linear probing
@@ -39,16 +67,44 @@ namespace lab9 {
          *
          * Create a hash table with an initial size of 100
          */
+
     }
 
     hash_table::~hash_table() {
-
+        delete[] hash_table_array;
     }
 
     bool hash_table::insert(std::string key, int value) {
         // Insert a key according to the defined probing technique
         // If you run into an issue where you get an infinite collision loop,
         //   figure out a way to get out of the loop.
+        if(current_size*1.0/max_size > 0.7){
+            expand();
+        }
+        int attempt = 0;
+        int h1 = hash_1(key);
+        int h2 = 0;
+        if(probing == 'd'){
+            h2 = hash_2(key);
+        }
+        int a = 0;
+        while(attempt < max_size){
+            if(hash_table_array[(h1+a)%(max_size)].key == ""){
+                hash_table_array[(h1+a)%(max_size)].key = key;
+                hash_table_array[(h1+a)%(max_size)].value = value;
+                break;
+            }
+            ++attempt;
+            a = attempt;
+            if(probing == 'q'){
+                a = a*a;
+            }else if(probing == 'd'){
+                a = a * h2;
+            }
+        }
+        if(attempt >= max_size){
+            return false;
+        }
         return true;
     }
 
@@ -56,12 +112,57 @@ namespace lab9 {
         // Checks to see if that key is in the table.
         // Use the specified probing technique
         // Keep collisions in mind
+        int attempt = 0;
+        int h1 = hash_1(key);
+        int h2 = 0;
+        if(probing == 'd'){
+            h2 = hash_2(key);
+        }
+        int a = 0;
+        while(attempt < max_size){
+            if(hash_table_array[(h1+a)%(max_size)].key == key){
+                break;
+            }
+            ++attempt;
+            a = attempt;
+            if(probing == 'q'){
+                a = a*a;
+            }else if(probing == 'd'){
+                a = a * h2;
+            }
+        }
+        if(attempt >= max_size){
+            return false;
+        }
         return true;
+
     }
 
     int hash_table::get(std::string key) {
         // Get the int value from the node that has key
         // Use the specified probing technique
+        int attempt = 0;
+        int h1 = hash_1(key);
+        int h2 = 0;
+        if(probing == 'd'){
+            h2 = hash_2(key);
+        }
+        int a = 0;
+        while(attempt < max_size){
+            if(hash_table_array[(h1+a)%(max_size)].key == key){
+                break;
+            }
+            ++attempt;
+            a = attempt;
+            if(probing == 'q'){
+                a = a*a;
+            }else if(probing == 'd'){
+                a = a * h2;
+            }
+        }
+        if(attempt < max_size){
+            return hash_table_array[(h1+a)%(max_size)].value;
+        }
         return 0;
     }
 
@@ -69,17 +170,71 @@ namespace lab9 {
         // Update a key in the hash table.
         // Keep collisions in mind
         // Use the specified probing technique
+        int attempt = 0;
+        int h1 = hash_1(key);
+        int h2 = 0;
+        if(probing == 'd'){
+            h2 = hash_2(key);
+        }
+        int a = 0;
+        while(attempt < max_size){
+            if(hash_table_array[(h1+a)%(max_size)].key == key){
+                break;
+            }
+            ++attempt;
+            a = attempt;
+            if(probing == 'q'){
+                a = a*a;
+            }else if(probing == 'd'){
+                a = a * h2;
+            }
+        }
+        if(attempt < max_size){
+            hash_table_array[(h1+a)%(max_size)].value = value;
+        }
+
     }
 
     void hash_table::remove(std::string key){
         // Remove an item from the hash table. Keep collisions in mind
         // Use the specified probing technique
         // If the item isn't in the table, do nothing.
+        int attempt = 0;
+        int h1 = hash_1(key);
+        int h2 = 0;
+        if(probing == 'd'){
+            h2 = hash_2(key);
+        }
+        int a = 0;
+        while(attempt < max_size){
+            if(hash_table_array[(h1+a)%(max_size)].key == key){
+                break;
+            }
+            ++attempt;
+            a = attempt;
+            if(probing == 'q'){
+                a = a*a;
+            }else if(probing == 'd'){
+                a = a * h2;
+            }
+        }
+        if(attempt < max_size){
+            hash_table_array[(h1+a)%(max_size)].key = "";
+        }
+        --current_size;
     }
 
     std::string hash_table::to_string(){
         // Run through the entire array and create a string following this format. The {} brackets aren't part of the return
         // [{array location}]{key_value.key}:{key_value.int}
+        std::string output;
+        for(int i = 0; i < max_size; ++i){
+            if(hash_table_array[i].key != ""){
+                output += "["+std::to_string(i)+"]"+ hash_table_array[i].key +":"+std::to_string(hash_table_array[i].value);
+                output += "\n";
+            }
+        }
+        return output;
         return std::string();
     }
 }
--- lib/lab09/src/sorts.cpp
+++ lib/lab09/src/sorts.cpp
@@ -2,29 +2,77 @@
 
 namespace lab6 {
     // Auxiliary Functions
-    unsigned partition(int* to_sort, int low, int high);
+    unsigned partititon(int* to_sort, int low, int high);
 
     doubly_linked_list recursive_merge_sort(doubly_linked_list to_sort);
     doubly_linked_list merge(doubly_linked_list left, doubly_linked_list right);
 
     void quicksort(int* to_sort, int low, int high){
-
+        if(high <= low){
+            return;
+        }
+        int part = partititon(to_sort,low,high);
+        quicksort(to_sort,low,part-1);
+        quicksort(to_sort,part+1,high);
     }
 
     unsigned partititon(int* to_sort, int low, int high){
-
+        int mid = low +((high-low) >> 1);
+        int pivot = to_sort[mid];
+        int left = low;
+        int right = high;
+        int tr = left;
+        while(tr<right){
+            if(to_sort[tr] < pivot){
+                std::swap(to_sort[left],to_sort[tr]);
+                ++left;
+                ++tr;
+            }else if(to_sort[tr] > pivot){
+                std::swap(to_sort[tr],to_sort[right]);
+                --right;
+            }else{
+                ++tr;
+            }
+        }
+        return left;
     }
 
     void mergesort(doubly_linked_list& to_sort){
-
+        to_sort = recursive_merge_sort(to_sort);
     }
 
     doubly_linked_list recursive_merge_sort(doubly_linked_list to_sort){
-
+        int size = to_sort.size();
+        if(size <= 1){
+            return to_sort;
+        }
+        int split = size/2;
+        doubly_linked_list another = to_sort.split(split);
+        to_sort = recursive_merge_sort(to_sort);
+        another = recursive_merge_sort(another);
+        return merge(to_sort,another);
     }
 
     //Used for the merging the two lists
     doubly_linked_list merge(doubly_linked_list left, doubly_linked_list right){
-
+        doubly_linked_list newlist;
+        while(!left.is_empty()&&!right.is_empty()){
+            if(left.get_data(0) <= right.get_data(0)){
+                newlist.append(left.get_data(0));
+                left.remove(0);
+            }else{
+                newlist.append(left.get_data(0));
+                right.remove(0);
+            }
+        }
+        while(!left.is_empty()){
+            newlist.append(left.get_data(0));
+            left.remove(0);
+        }
+        while(!right.is_empty()){
+            newlist.append(right.get_data(0));
+            right.remove(0);
+        }
+        return newlist;
     }
 }
